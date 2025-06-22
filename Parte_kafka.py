9a57da7426b665fcafbf150319a469a609ee4ad0
Thomas Salazar <134440162+Thomas-thief@users.noreply.github.com>
2025-06-21 22:25:37 -0400

    Mejora en la extracción de datos en avro
    ahora considera la metadata necesaria para el modelo, aunque todavía no funciona correctamente. pero la estructura es esta.

--- Parte_kafka.py
+++ Parte_kafka.py
@@ -4,35 +4,71 @@ from fastavro import reader
 import gzip
 import io
 from astropy.io import fits
+import struct
+"""
+la forma de ejecutar esto es con la maquina virtual de mi pc (Thomas)
+con 2 temrinales en wsl se deben de ejecutar estas 2 lineas de comandos en la carpeta de kafka
 
+bin/zookeeper-server-start.sh config/zookeeper.properties
+
+bin/kafka-server-start.sh config/server.properties
+
+
+"""
 files_path = ".\\datos\\"
 onlyfiles = [files_path + f for f in listdir(files_path)]
 #print(onlyfiles)
+metadata = ["ra","dec","magpsf","sigmapsf","isdiffpos","diffmaglim","fwhm","sgscore1","sgscore2","sgscore3",
+            "distpsnr1","distpsnr2","distpsnr3","classtar","ndethist","ncovhist","chinr","sharpnr","approx_nondet",
+            "gal_lat","gal_lng","ecl_lat","ecl_lng"]
 
-
-# Kafka topic
 PRODUCER = KafkaProducer(bootstrap_servers="172.28.190.252", value_serializer=lambda v: v)
-TOPIC = "Imagen"
 
 for file in onlyfiles:
     with open(file, 'rb') as f:
         avro_reader = reader(f)
+
         for record in avro_reader:
-            # Accede a los cutouts
-            for name in ["cutoutScience", "cutoutTemplate", "cutoutDifference"]:
+            for name in ["objectId", "candidate", "cutoutScience", "cutoutTemplate", "cutoutDifference"]:
                 cutout = record[name]
 
+                if name == "objectId":
+                    #print(cutout) # send
+                    PRODUCER.send("Id", cutout.encode('utf-8'))
+
+                elif name == "candidate":
+                    for key in metadata:
+                        if key in ["gal_lat","gal_lng","ecl_lat","ecl_lng"]:
+                            if key in record.keys():
+                                #print(record[key]) #send
+                                PRODUCER.send("Metadata", record[key].tobytes())
+                            else:
+                                #print(0) #send Nan
+                                cero = 0
+                                PRODUCER.send("Metadata", cero.tobytes())
+
+                        elif key == "approx_nondet":
+                            #print(cutout.get("ncovhist") - cutout["ndethist"]) #send
+                            resta = cutout.get("ncovhist") - cutout["ndethist"]
+                            PRODUCER.send("Metadata", struct.pack('d', resta))
+
+                        else:
+                            print(cutout[key]) #send
+                            if type(cutout[key]) == type(0.1):
+                                PRODUCER.send("Metadata", struct.pack('d', cutout[key]))
+                            elif type(cutout[key]) == type(" "):
+                                PRODUCER.send("Metadata", cutout[key].encode('utf-8'))
+
                 if 'stampData' in cutout:
-                    # Extraer y descomprimir los datos gzip
+
                     compressed_data = cutout['stampData']
                     with gzip.GzipFile(fileobj=io.BytesIO(compressed_data)) as gz:
                         hdulist = fits.open(gz)
                         image_data = hdulist[0].data
-                        PRODUCER.send(TOPIC, image_data.tobytes())
-                        # Mostrar la imagen
+                        PRODUCER.send("Imagen", image_data.tobytes())
+
                     print(name, file)
                 else:
                     print(f"{name} no contiene datos de imagen.")
             
-            break  # Solo el primer registro
-
+            break 
