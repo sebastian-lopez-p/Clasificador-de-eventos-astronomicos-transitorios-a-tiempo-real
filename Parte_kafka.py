7039d6f4ee987a98716781e7ef876ab856b78fd7
Thomas Salazar <134440162+Thomas-thief@users.noreply.github.com>
2025-06-24 11:39:14 -0400

    Actualización de Parte_kafka.py
    se modifico como se envían los datos del producer, ahora tienen una key, es decir la ID

--- Parte_kafka.py
+++ Parte_kafka.py
@@ -13,12 +13,11 @@ con 2 temrinales en wsl se deben de ejecutar estas 2 lineas de comandos en la ca
 bin/zookeeper-server-start.sh config/zookeeper.properties
 
 bin/kafka-server-start.sh config/server.properties
-
-
 """
+
 files_path = ".\\datos\\"
 onlyfiles = [files_path + f for f in listdir(files_path)]
-#print(onlyfiles)
+
 metadata = ["ra","dec","magpsf","sigmapsf","isdiffpos","diffmaglim","fwhm","sgscore1","sgscore2","sgscore3",
             "distpsnr1","distpsnr2","distpsnr3","classtar","ndethist","ncovhist","chinr","sharpnr","approx_nondet",
             "gal_lat","gal_lng","ecl_lat","ecl_lng"]
@@ -30,37 +29,33 @@ for file in onlyfiles:
         avro_reader = reader(f)
 
         for record in avro_reader:
-            for name in ["objectId", "candidate", "cutoutScience", "cutoutTemplate", "cutoutDifference"]:
+            for name in ["candidate", "cutoutScience", "cutoutTemplate", "cutoutDifference"]:
                 cutout = record[name]
-
-                if name == "objectId":
-                    #print(cutout) # send
-                    PRODUCER.send("Id", cutout.encode('utf-8'))
-
-                elif name == "candidate":
+                ID = record["objectId"].encode('utf-8')
+                if name == "candidate":
                     for key in metadata:
                         if key in ["gal_lat","gal_lng","ecl_lat","ecl_lng"]:
                             if key in record.keys():
-                                #print(record[key]) #send
-                                PRODUCER.send("Metadata", record[key].to_bytes())
+                                print(record[key]) #send
+                                PRODUCER.send("Metadata", record[key].to_bytes(), ID)
 
                             else:
-                                #print(0) #send Nan
+                                print(0) #send Nan
                                 cero = 0
-                                PRODUCER.send("Metadata", cero.to_bytes())
+                                PRODUCER.send("Metadata", cero.to_bytes(), ID)
 
                         elif key == "approx_nondet":
-                            #print(cutout.get("ncovhist") - cutout["ndethist"]) #send
+                            print(cutout.get("ncovhist") - cutout["ndethist"]) #send
                             resta = cutout.get("ncovhist") - cutout["ndethist"]
-                            PRODUCER.send("Metadata", struct.pack('d', resta))
+                            PRODUCER.send("Metadata", struct.pack('d', resta), ID)
 
                         else:
                             print(cutout[key]) #send
                             if type(cutout[key]) == type(0.1):
-                                PRODUCER.send("Metadata", struct.pack('d', cutout[key]))
-                                
+                                PRODUCER.send("Metadata", struct.pack('d', cutout[key]), ID)
+
                             elif type(cutout[key]) == type(" "):
-                                PRODUCER.send("Metadata", cutout[key].encode('utf-8'))
+                                PRODUCER.send("Metadata", cutout[key].encode('utf-8'), ID)
 
                 if 'stampData' in cutout:
 
@@ -68,9 +63,8 @@ for file in onlyfiles:
                     with gzip.GzipFile(fileobj=io.BytesIO(compressed_data)) as gz:
                         hdulist = fits.open(gz)
                         image_data = hdulist[0].data
-                        PRODUCER.send("Imagen", image_data.tobytes())
+                        PRODUCER.send("Imagen", image_data.tobytes(), ID)
 
                     print(name, file)
-                else:
-                    print(f"{name} no contiene datos de imagen.")
-    time.sleep(8)        
+    PRODUCER.flush()
+    time.sleep(8)
